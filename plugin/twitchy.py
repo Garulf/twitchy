ddb0d9b26f70d6a613d38dc49305e5ec1ee0a7da
Garulf <535299+Garulf@users.noreply.github.com>
2021-08-18 02:44:34 -0400

    Update twitchy.py

--- plugin/twitchy.py
+++ plugin/twitchy.py
@@ -2,9 +2,9 @@ import os
 import tempfile
 import webbrowser
 
-from flox import Flox
+from flox import Flox, ICON_SETTINGS, ICON_APP_ERROR, ICON_CANCEL
 import requests
-from twitch import TwitchClient
+from twitch import TwitchClient, client
 from streamlink import streams
 
 from requests.exceptions import HTTPError
@@ -22,11 +22,11 @@ class Twitchy(Flox):
     @property
     def client(self):
         if self._client is None:
-            self._client = TwitchClient(self.settings['client_id'])
+            self._client = TwitchClient(self.settings.get('client_id'))
             try:
                 self.client.ingests.get_server_list()
             except HTTPError:
-                self.logger.error("Login failed!")
+                pass
         return self._client
 
     def search(self, query):
@@ -35,6 +35,14 @@ class Twitchy(Flox):
             self.add_streams(channels)
         except TypeError:
             pass
+        except HTTPError as e:
+            self._add_except(e)
+            self.add_item(
+                title="Reset client ID",
+                subtitle="This will wipe your client ID from settings.",
+                icon=ICON_CANCEL,
+                method='reset_settings'
+            )
 
     @staticmethod
     def sort_channels(channel):
@@ -89,9 +97,35 @@ class Twitchy(Flox):
         return follows
 
     def query(self, query):
-        slugged_query = query.lower().replace(' ', '-')
-        if query != '':
-            self.search(query)
+        if not self.settings.get("client_id") or self.settings.get("client_id") == "":
+            self.add_item(
+                title=query,
+                subtitle="Please enter your Twitch client token.",
+                icon=ICON_SETTINGS,
+                method='set_client_id',
+                parameters=[query],
+                hide=True
+            )
+        else:
+            if query != '':
+                self.search(query)
     
+    def reset_settings(self):
+        self.settings['_client_id'] = self.settings['client_id']
+        self.settings['client_id'] = ""
+
+    def set_client_id(self, client_id):
+        try:
+            self._client = TwitchClient(client_id)
+            self.client.ingests.get_server_list()
+        except HTTPError:
+            self.show_msg(f"{self.name}: Client ID failed!", "Failed to connect to Twitch using client ID! Please try again!")
+            self.settings['client_id'] = ""
+        else:
+            self.settings['client_id'] = client_id
+            # self.show_msg(f"{self.name}: Success!!", f"{self.name} is now logged in!")
+            self.change_query(f"{self.user_keyword} ", requery=True)
+
+
     def open_url(self, url):
         webbrowser.open(url)
