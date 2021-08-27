f402556081fc2a12210cece9ebcef67846b9065e
Garulf <535299+Garulf@users.noreply.github.com>
2021-08-27 00:12:48 -0400

    remove unused imports

--- plugin/twitchy.py
+++ plugin/twitchy.py
@@ -3,9 +3,8 @@ import tempfile
 import webbrowser
 
 from flox import Flox, ICON_SETTINGS, ICON_APP_ERROR, ICON_CANCEL
-import requests
 from twitch import TwitchClient, client
-from streamlink import streams
+# from streamlink import streams
 
 from requests.exceptions import HTTPError
 
